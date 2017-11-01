96de871a755794c39e569cbda67c5f149bd72871
jiedo <jiedohh@gmail.com>
2017-11-01 20:13:55 +0800

    fix: dump ldoce import

--- dump_ldoce.py
+++ dump_ldoce.py
@@ -12,11 +12,11 @@ def page():
         print (c.attrs, c.text.replace("\n\n", "\n").replace("\n\n", "\n").replace("\n", "\n\t").encode("utf8"))
     print ()
 
-    # for c in root.find_all("div"):
-    #     print c.attrs, c.text.replace("\n\n", "\n").replace("\n\n", "\n").replace("\n", "\n\t").encode("utf8")
-    # print
+    for c in root.find_all("div"):
+        print (c.attrs, c.text.replace("\n\n", "\n").replace("\n\n", "\n").replace("\n", "\n\t").encode("utf8"))
+    print ()
 
-    # print data
+    # print (data)
 
     # print(root.prettify().encode("utf8"))
     # for child in root:
@@ -24,7 +24,7 @@ def page():
 
 def search():
     from utils.text import enc_utf8
-    import utils.fulltext import Searcher
+    from utils.fulltext import Searcher
     from utils.advanced import search_and_render
 
     url = "search:///?filters=(asfilter:233)&mode=headwords"
@@ -34,11 +34,11 @@ def search():
     searcher_hp = Searcher(fulltext_hwdphr_path, variations_path)
     searcher_de = Searcher(fulltext_defexa_path, variations_path)
     data = enc_utf8(search_and_render("headwords", "", "(asfilter:233)", searcher_hp, searcher_de))
-    word1000 = {}
+    word1000 = []
     root = BeautifulSoup(data, "lxml")
     for c in root.find_all("a"):
-        word1000[c.text] = c.attrs['href']
+        word1000 += [(c.text, c.attrs['href'])]
         # print (c.attrs, c.text)
     print (json.dumps(word1000, indent=2))
 
-page()
+search()
